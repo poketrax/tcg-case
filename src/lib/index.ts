df8afc9120ac9709e5f71ac48b76aa77cf6cebd5
jgunzelman <jgunzelman88@gmail.com>
2022-11-02 08:30:08 -0400

    📦

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export * from "./CardCase";
 export * from "./CardGradeStamp";
 export * from "./CardImage";
+export * from "./CardImageFancy";
 export * from "./Energy";
 export * from "./PokeRarity";
 export * from "./Price";
